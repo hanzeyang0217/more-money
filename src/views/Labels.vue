1d29e1f34a0a734033c988771e71dfccca128083
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-30 01:18:21 +0900

    è创建路由

---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Labels</div>
+</template>
+
+<script>
+  export default {
+    name: "Labels"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Money</div>
+</template>
+
+<script>
+  export default {
+    name: "Money"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

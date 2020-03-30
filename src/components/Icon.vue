866ed0dadd5be338b7dd3fa305c943065948d884
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-30 23:48:09 +0900

    添加icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#'+this.name"></use>
+  </svg>
+</template>
+
+<script lang="js">
+  //搜来的
+  const importAll = (requireContext) => requireContext.keys().forEach(requireContext)
+  importAll(require.context('@/assets/icons/', true, /\.svg$/))
+  export default {
+    props: ['name'],
+    name: "Icon"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .icon {
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    overflow: hidden;
+  }
+</style>

7c79a7a98685b2e3a42b3582f4d88540f2145222
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-31 23:30:54 +0900

    money-numberPad组件 js部分完成

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,33 +1,72 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{btOutput}}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="enterNumber(1)">1</button>
+      <button @click="enterNumber(2)">2</button>
+      <button @click="enterNumber(3)">3</button>
+      <button @click="deleteEnter()">删除</button>
+      <button @click="enterNumber(4)">4</button>
+      <button @click="enterNumber(5)">5</button>
+      <button @click="enterNumber(6)">6</button>
+      <button @click="clearEnter()">清空</button>
+      <button @click="enterNumber(7)">7</button>
+      <button @click="enterNumber(8)">8</button>
+      <button @click="enterNumber(9)">9</button>
+      <button @click="submitEnter()" class="ok">OK</button>
+      <button @click="enterNumber(0)" class="zero">0</button>
     </div>
   </div>
 </template>
 
 <script lang="js">
   export default {
-    name: "NumberPad"
+    name: "NumberPad",
+    data() {
+      return {
+        output: 0
+      }
+    },
+    computed: {
+      btOutput: function () {
+        if (this.output.toString().length <= 3) {
+          return this.output
+        } else if (this.output.length <= 7) {
+          return Number(this.output).toLocaleString()
+        } else {
+          return `not support rich`
+        }
+      }
+    },
+    methods: {
+      enterNumber(number) {
+        if (this.output === 0) {
+          this.output = number
+        } else {
+          this.output = this.output.toString() + number
+        }
+      },
+      deleteEnter() {
+        if (this.output.toString().length === 1) {
+          this.output = 0
+        } else {
+          this.output = this.output.toString().slice(0, -1)
+        }
+      },
+      clearEnter() {
+        this.output = 0
+      },
+      submitEnter() {
+        //TODO
+      }
+    }
   }
+
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
+
   .numberPad {
     .output {
       @extend %clearFix;
@@ -37,40 +76,52 @@
       padding: 9px 16px;
       text-align: right;
     }
+
     .buttons {
       @extend %clearFix;
+
       > button {
         width: 25%;
         height: 64px;
         float: left;
         background: transparent;
         border: none;
+
         &.ok {
           height: 64*2px;
           float: right;
         }
+
         &.zero {
-          width: 25*2%;
+          width: 25*3%;
         }
+
         $bg: #f2f2f2;
+
         &:nth-child(1) {
           background: $bg;
         }
+
         &:nth-child(2), &:nth-child(5) {
           background: darken($bg, 4%);
         }
+
         &:nth-child(3), &:nth-child(6), &:nth-child(9) {
           background: darken($bg, 4*2%);
         }
+
         &:nth-child(4), &:nth-child(7), &:nth-child(10) {
           background: darken($bg, 4*3%);
         }
+
         &:nth-child(8), &:nth-child(11), &:nth-child(13) {
           background: darken($bg, 4*4%);
         }
+
         &:nth-child(14) {
           background: darken($bg, 4*5%);
         }
+
         &:nth-child(12) {
           background: darken($bg, 4*6%);
         }

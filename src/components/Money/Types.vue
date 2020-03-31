96d81d9f7ef16ee99b97ecba429476a42157167c
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-31 22:11:41 +0900

    money-type组件 js部分完成

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type==='-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type==='+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
 <script lang="js">
   export default {
-    name: "Types"
+    name: "Types",
+    data() {
+      return {
+        type: '-' // - 为支出 + 为收入
+      }
+    },
+    methods: {
+      selectType(type) {
+        if (type !== '-' && type !== '+') {
+          throw new Error('unknown type')
+        }
+        this.type = type
+      }
+    }
   }
 </script>
 
@@ -19,13 +32,16 @@
     display: flex;
     text-align: center;
     font-size: 24px;
+
     > li {
+      cursor: pointer;
       width: 50%;
       height: 64px;
       display: flex;
       justify-content: center;
       align-items: center;
       position: relative;
+
       &.selected::after {
         content: '';
         position: absolute;
